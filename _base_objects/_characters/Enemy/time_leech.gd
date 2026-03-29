2fa59a0469c1d877bf023b41efcf4c86f40a2236
varshzakalq <varshtiwari53@gmail.com>
2026-03-29 15:38:23 +0530

    level 1 done

--- _base_objects/_characters/Enemy/time_leech.gd
+++ _base_objects/_characters/Enemy/time_leech.gd
@@ -21,7 +21,7 @@ func _physics_process(delta: float) -> void:
 	if not navigation_agent_3d.is_navigation_finished():
 		var next_path_pos = navigation_agent_3d.get_next_path_position()
 		var direction = (next_path_pos - global_position).normalized()
-		
+		look_at(Vector3(next_path_pos.x, global_position.y, next_path_pos.z ), Vector3.UP)
 		velocity = direction * 2.0
 
 		move_and_slide()
